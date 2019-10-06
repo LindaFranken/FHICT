4b37707de7ad3e52b4b757f863dc000ca34dba6e
Linda <linda.franken@student.fontys.nl>
2019-10-06 21:52:34 +0200

    project

---
+++ Individual/Individual/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Individual
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
